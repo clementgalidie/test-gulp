02172ede074e08aa29ae427b8e219361780209ea
Clément Galidie <galidie.clement@gmail.com>
2015-05-23 16:35:37 +0200

    Reorganize whole tree view + gulpfile assets paths

--- gulpfile.js
+++ gulpfile.js
@@ -4,49 +4,64 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
+var baseFolders = {
+  dev: 'src/',
+  dist: 'dist/'
+};
+
+var devFolders = {
+  css: baseFolders.dev + 'css/',
+  js: baseFolders.dev + 'js/'
+};
+
+var distFolders = {
+  css: baseFolders.dist + 'css/',
+  js: baseFolders.dist + 'js/'
+};
+
 // Task to set up a local server + livereload (without the use of a browser plugin).
 gulp.task('connect', function () {
   plugins.connect.server({
-    root: '.',
+    root: 'src/',
     port: 1337,
     livereload: true
   });
 });
 
-// Task to minify html files.
+// Task to lint html files.
 gulp.task('html', function () {
-  gulp.src('*.html')
+  return gulp.src(baseFolders.dev + '*.html')
     .pipe(plugins.minifyHtml())
     .pipe(plugins.connect.reload());
 });
 
 // Task to compile sass into css files.
 gulp.task('sass', function () {
-  gulp.src('src/sass/**/*.scss')
+  return gulp.src(devFolders.css + 'sass/**/*.scss')
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.sass({ style: 'expanded' }))
     .pipe(plugins.minifyCss())
     .pipe(plugins.sourcemaps.write())
     .pipe(plugins.rename('main.min.css'))
-    .pipe(gulp.dest('assets/css/'))
+    .pipe(gulp.dest(devFolders.css))
     .pipe(plugins.connect.reload());
 });
 
 // Task to uglify js files.
 gulp.task('js', function () {
-  gulp.src('src/scripts/**/*.js')
-    .pipe(plugins.uglify())
+  gulp.src(devFolders.js + 'scripts/**/*.js')
     .pipe(plugins.concat('main.js'))
+    .pipe(plugins.uglify())
     .pipe(plugins.rename('main.min.js'))
-    .pipe(gulp.dest('assets/js/'))
+    .pipe(gulp.dest(devFolders.js))
     .pipe(plugins.connect.reload());
 });
 
 // Task to watch for file changes.
 gulp.task('watch', function () {
-  gulp.watch('*.html', ['html']);
-  gulp.watch('src/sass/**/*.scss', ['sass']);
-  gulp.watch('src/scripts/**/*.js', ['js']);
+  gulp.watch(baseFolders.dev + '*.html', ['html']);
+  gulp.watch(devFolders.css + '**/*.scss', ['sass']);
+  gulp.watch(devFolders.js + '**/*.js', ['js']);
 });
 
 // Default task, run it by typing 'gulp' in CLI.
